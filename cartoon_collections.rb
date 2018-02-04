1bd82e615ec39ceec4b9cad2dafd7a9344939e3e
Alec German <alec_german@yahoo.com>
2018-02-04 02:32:10 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,5 +17,5 @@ def find_the_cheese(cheeses)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 
-  cheeses.select { |item| cheese_types.include?(item)}
+  cheese_types.select { |item| cheese.include?(item)}
 end
